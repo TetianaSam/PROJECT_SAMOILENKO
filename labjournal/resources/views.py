a96db73f9ee878b849468eb1a4b8b8a69ed4c8c5
TetianaSam <samoilenkotanya1989@gmail.com>
2024-08-27 18:06:06 +0200

    styly

--- labjournal/resources/views.py
+++ labjournal/resources/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Resource, Suggestion
-from .forms import ResourceForm, SuggestionForm
+from .forms import ResourceForm, SuggestionForm, SearchForm
 from django.contrib.auth.decorators import login_required
 
 

1f4dbb587067ca96a31debc44979619311aa84d7
TetianaSam <samoilenkotanya1989@gmail.com>
2024-09-08 13:33:28 +0200

    back

--- labjournal/labjournal/settings.py
+++ labjournal/labjournal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-usg^=srf*-zq1zv#e_qr4qs^)n)!4*z9-bjh5hiar4t8jcf^th
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['yourusername.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 
@@ -125,10 +125,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
